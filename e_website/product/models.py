52c1bdd873c0b570ef6255af6871e06c5992710c
BHIlluminati004 <bhavyahari1209@gmail.com>
2025-06-27 08:51:40 +0530

    creating DB

--- e_website/product/models.py
+++ e_website/product/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    Name=models.CharField(max_length=100)
+    Description=models.TextField()
+    Price=models.IntegerField()
