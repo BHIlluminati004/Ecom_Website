920c439afc7dce64b809624de38bef2148d02945
BHIlluminati004 <bhavyahari1209@gmail.com>
2025-06-29 11:15:31 +0530

    Creating a DB

--- e_website/product/models.py
+++ e_website/product/models.py
@@ -5,3 +5,8 @@ class Product(models.Model):
     Name=models.CharField(max_length=100)
     Description=models.TextField()
     Price=models.IntegerField()
+
+class Image(models.Model):
+    Wildphotography=models.CharField(max_length=500)
+        
+        
