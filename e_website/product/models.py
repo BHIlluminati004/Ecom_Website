ca09d712c1e053943ccb8ce12c7bc5143f3e9af6
BHIlluminati004 <bhavyahari1209@gmail.com>
2025-06-30 11:07:16 +0530

    update

--- e_website/product/models.py
+++ e_website/product/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     Name=models.CharField(max_length=100)
     Description=models.TextField()
     Price=models.IntegerField()
+
+    def __str__(self):
+        return self.Name
